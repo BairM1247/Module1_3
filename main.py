8fda8aa6ce008913686eac2fc4c1544f2759e390
BairM1247 <mba84@mail.ru>
2024-09-04 21:43:02 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+name = "Bair"
+print ('Name: ' + name)
+age = 40
+print ('Age: ' + str (age))
+age = age + 1
+print ('New Age: ' + str (age))
+is_student = "Is Student"
+print ('Is Student: ',"Is Student".istitle() )
